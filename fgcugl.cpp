d310523a2064829ef3495841009d6825a6d5e43a
Paul Allen <pallen@fgcu.edu>
2021-01-29 10:05:10 -0500

    add ifdef for macos

--- fgcugl.cpp
+++ fgcugl.cpp
@@ -38,9 +38,11 @@ namespace fgcugl
 
 
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+#ifdef __APPLE__
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	// for macos
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
 		if (resizable)
 			glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
